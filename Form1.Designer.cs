f95e51b09d573c2f8a8e3964e404e4fb5b3136ed
spots97 <nictoon@gmail.com>
2021-01-26 23:19:30 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace DamnTabs
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.textBoxFile = new System.Windows.Forms.TextBox();
+            this.comboBoxMode = new System.Windows.Forms.ComboBox();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Indent Mode:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(452, 6);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(34, 23);
+            this.buttonBrowse.TabIndex = 2;
+            this.buttonBrowse.Text = "...";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // textBoxFile
+            // 
+            this.textBoxFile.AllowDrop = true;
+            this.textBoxFile.Location = new System.Drawing.Point(46, 6);
+            this.textBoxFile.Name = "textBoxFile";
+            this.textBoxFile.Size = new System.Drawing.Size(400, 23);
+            this.textBoxFile.TabIndex = 3;
+            this.textBoxFile.TextChanged += new System.EventHandler(this.ReadyUp);
+            // 
+            // comboBoxMode
+            // 
+            this.comboBoxMode.FormattingEnabled = true;
+            this.comboBoxMode.Items.AddRange(new object[] {
+            "Indented",
+            "Tabbed"});
+            this.comboBoxMode.Location = new System.Drawing.Point(96, 35);
+            this.comboBoxMode.Name = "comboBoxMode";
+            this.comboBoxMode.Size = new System.Drawing.Size(121, 23);
+            this.comboBoxMode.TabIndex = 4;
+            this.comboBoxMode.Text = "Select Mode";
+            this.comboBoxMode.SelectedIndexChanged += new System.EventHandler(this.ReadyUp);
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(12, 70);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(474, 23);
+            this.buttonRun.TabIndex = 5;
+            this.buttonRun.Text = "Run";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.statusStrip1.Location = new System.Drawing.Point(0, 107);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(500, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(39, 17);
+            this.toolStripStatusLabel1.Text = "Ready";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Overflow = System.Windows.Forms.ToolStripItemOverflow.Never;
+            this.toolStripProgressBar1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(350, 16);
+            this.toolStripProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.toolStripProgressBar1.Value = 50;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 129);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.buttonRun);
+            this.Controls.Add(this.comboBoxMode);
+            this.Controls.Add(this.textBoxFile);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Danm Tabs!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.TextBox textBoxFile;
+        private System.Windows.Forms.ComboBox comboBoxMode;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace DamnTabs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            using var d = new OpenFileDialog()
+            {
+                Filter = "Word Document (*.docx)|*.docx",
+                ValidateNames = true
+            };
+            d.FileOk += openFileDialog_FileOk;
+            d.ShowDialog();
+        }
+
+        private void openFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            if (!e.Cancel)
+            {
+                textBoxFile.Text = ((OpenFileDialog)sender).FileName;
+                textBoxFile.BackColor = System.Drawing.Color.White;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxMode.SelectedIndex = 0;
+            toolStripProgressBar1.Visible = false;
+            toolStripProgressBar1.Value = toolStripProgressBar1.Minimum;
+        }
+
+        private void Run()
+        {
+            // This is the path to the new file not the old one.
+            var path = $"{Path.GetDirectoryName(textBoxFile.Text)}\\{Path.GetFileNameWithoutExtension(textBoxFile.Text)}-{comboBoxMode.Text}_DamnTabs.docx";
+
+            toolStripStatusLabel1.Text = "Duplicating Document";
+            // Dupe the file
+            {
+                int i = 0;
+                while (File.Exists(path))
+                {
+                    i++;
+                    path = $"{Path.GetDirectoryName(textBoxFile.Text)}\\{Path.GetFileNameWithoutExtension(textBoxFile.Text)}-{comboBoxMode.Text} {i}_DamnTabs.docx";
+                }
+                File.Copy(textBoxFile.Text, path);
+                File.SetLastWriteTimeUtc(path, DateTime.UtcNow);
+            }
+
+
+            // Get the Doc
+            toolStripStatusLabel1.Text = "Opening...";
+            using FileStream docx = new FileStream(path, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            using var wordDoc = WordprocessingDocument.Open(docx, true);
+            var doc = wordDoc.MainDocumentPart.Document;
+
+            // Fix the doc
+            if (comboBoxMode.SelectedIndex == 0)
+            {
+                ReplaceTabs(doc);
+            }
+            else if (comboBoxMode.SelectedIndex == 1)
+            {
+                toolStripStatusLabel1.Text = "Scanning Document...";
+                // Get all indent nodes
+                var ps = from bodyChild in doc.Body
+                         where bodyChild is Paragraph
+                         let p = (Paragraph)bodyChild                       // WE MAKE NO ASSUMPTIONS:
+                         where p.GetFirstChild<Run>() != null               // Checks if a run exists
+                         && p.ParagraphProperties != null                   // Checks if w:pPr exists
+                         && p.ParagraphProperties.Indentation != null       // Checks if w:ind exists
+                         //&& p.ParagraphProperties.Indentation.HasAttributes // Checks if w:ind has Attributes
+                         //from attr in p.ParagraphProperties.Indentation.GetAttributes()
+                         //where attr.LocalName == "firstLine"
+                         && p.ParagraphProperties.Indentation.FirstLine != null
+                         select p;
+
+                // Set up Progress Bar
+                toolStripStatusLabel1.Text = "Running";
+                var progMax = ps.Count();
+                InitProgressBar(progMax > 0 ? progMax : 1);
+
+                // Fix
+                foreach (var p in ps)
+                {
+                    var firstRun = p.GetFirstChild<Run>();
+                    var rpr = firstRun.GetFirstChild<RunProperties>();
+
+                    if (rpr == null)
+                        firstRun.FirstChild.InsertBeforeSelf(new TabChar());
+                    else
+                        rpr.InsertAfterSelf(new TabChar());
+                    p.ParagraphProperties.Indentation.FirstLine = null;
+
+                    toolStripProgressBar1.Value++;
+                }
+                toolStripProgressBar1.Value = toolStripProgressBar1.Maximum;
+            }
+
+            // SAVE FILE
+            doc.Save();
+
+            toolStripStatusLabel1.Text = "Done!";
+        }
+
+        private void ReplaceTabs(Document doc)
+        {
+            // Init Progress Bar
+
+            // TODO: Can we improve?
+
+            var indTally = new Dictionary<string, int>(3);
+            toolStripStatusLabel1.Text = "Scanning Document (1/2)";
+            var inds = from bodyChild in doc.Body
+                       where bodyChild is Paragraph
+                       let p = (Paragraph)bodyChild                       // WE MAKE NO ASSUMPTIONS:
+                       where p.ParagraphProperties != null                // Checks if w:pPr exists
+                       && p.ParagraphProperties.Indentation != null       // Checks if w:ind exists
+                       //&& p.ParagraphProperties.Indentation.HasAttributes // Checks if w:ind has Attributes
+                       //from attr in p.ParagraphProperties.Indentation.GetAttributes()
+                       //where attr.LocalName == "firstLine"
+                       && p.ParagraphProperties.Indentation.FirstLine != null
+                       select p.ParagraphProperties.Indentation.FirstLine;
+            int indsCount = inds.Count();
+
+            var badRuns = from bodyChild in doc.Body
+                          where bodyChild is Paragraph
+                          let p = (Paragraph)bodyChild
+                          let r = p.GetFirstChild<Run>()
+                          where r != null
+                          let tab = r.GetFirstChild<TabChar>()
+                          where tab != null
+                          let t = r.GetFirstChild<Text>()
+                          where t == null || t.IsAfter(tab)
+                          select r;
+            int badRunsCount = badRuns.Count();
+
+            toolStripStatusLabel1.Text = "Scanning Document (2/2)";
+            InitProgressBar(indsCount + badRunsCount);
+
+            foreach (var ind in inds)
+            {
+                if (!indTally.ContainsKey(ind.Value))
+                    indTally.Add(ind.Value, 1);
+                else
+                    indTally[ind.Value]++;
+                toolStripProgressBar1.Value++;
+            }
+
+            string indVal;
+            {
+                string IndVal()
+                {
+                    KeyValuePair<string, int> max = indTally.FirstOrDefault();
+                    foreach (var kv in indTally)
+                    {
+                        if (max.Value < kv.Value)
+                            max = kv;
+                    }
+                    return max.Key;
+                };
+                indVal = IndVal();
+            }
+
+            toolStripStatusLabel1.Text = "Running";
+            foreach (var run in badRuns)
+            {
+                // Double Check if null and Remove tab
+                var tab = run.GetFirstChild<TabChar>();
+                if (tab != null) tab.Remove();
+
+                var p = ((Paragraph)run.Parent);
+
+                // Get/Create ParagraphProperties
+                ParagraphProperties ppr;
+                if (p.ParagraphProperties == null)
+                {
+                    ppr = new ParagraphProperties();
+                    p.ParagraphProperties = ppr;
+                }
+                else ppr = p.ParagraphProperties;
+
+                // Check if indentation is 
+                if (ppr.Indentation == null)
+                    ppr.Indentation = new Indentation();
+
+                if (!string.IsNullOrWhiteSpace(indVal))
+                    ppr.Indentation.FirstLine = indVal;
+                else 
+                    ppr.Indentation.FirstLine = null;
+                toolStripProgressBar1.Value++;
+            }
+            if (string.IsNullOrWhiteSpace(indVal))
+                MessageBox.Show(
+                    "We removed the tabs, but we couldn't find any direct style indents to replace them with. You can set up your indentation by editing your document's styles.",
+                    "Tab Removal",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+        }
+
+        private void HideProgressBar()
+        {
+            toolStripProgressBar1.Value = toolStripProgressBar1.Minimum;
+            toolStripProgressBar1.Visible = false;
+        }
+
+        private void InitProgressBar(int max)
+        {
+            // Set up Progress Bar
+            toolStripProgressBar1.Minimum = 0;
+            toolStripProgressBar1.Maximum = max;
+            toolStripProgressBar1.Value = 0;
+            toolStripProgressBar1.Visible = true;
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBoxFile.Text) && File.Exists(textBoxFile.Text))
+            {
+                Run();
+            }
+            else
+            {
+                textBoxFile.BackColor = System.Drawing.Color.LightCoral;
+                toolStripStatusLabel1.Text = "Invalid Fields";
+            }
+        }
+
+        private void ReadyUp(object sender, EventArgs e) => Ready();
+
+        private void Ready()
+        {
+            if (toolStripStatusLabel1.Text != "Ready")
+            {
+                HideProgressBar();
+                toolStripStatusLabel1.Text = "Ready";
+            }
+        }
+    }
+}
